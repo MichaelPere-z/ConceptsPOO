6ef236fb54e9180d19765e0769529da2e268061e
Michael <michael121399@gmail.com>
2022-11-06 23:38:50 -0400

    Finish date class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConceptsPOO;
+
+
+Console.WriteLine("Hello, World!");
+
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(2022, 12, 6));
+    Console.WriteLine(new Date(2022, 11, 6));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
