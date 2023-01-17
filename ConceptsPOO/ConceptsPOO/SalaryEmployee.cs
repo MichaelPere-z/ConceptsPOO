9ac6d0235aaca39c2b5364362528083a35a1a7ff
Michael <michael121399@gmail.com>
2023-01-16 23:15:23 -0400

    si

---
+++ ConceptsPOO/ConceptsPOO/SalaryEmployee.cs
@@ -0,0 +1,10 @@
+﻿namespace ConceptsPOO
+{
+    public class SalaryEmployee : Employee
+    {
+        public override decimal GetValueToPay()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
